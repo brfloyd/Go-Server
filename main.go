b48382dbcb02c624f200809cba7d3185bd7a3e7f
brettfloyd <brfloyd19@gmail.com>
2024-12-22 23:40:26 -0500

    rand stuff

--- main.go
+++ main.go
@@ -1,3 +1,35 @@
-//init commit 
+package main
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "got / request\n")
+	io.WriteString(w, "This is a test\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server is running on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Server is shut down")
+	} else if err != nil {
+		fmt.Println("Server is not shut down")
+		os.Exit(1)
+	}
+}
