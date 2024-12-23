a95c324b5fe72293f681d02536dce23b5f736d60
brettfloyd <brfloyd19@gmail.com>
2024-12-23 00:37:08 -0500

    server that does basic add sub

--- main.go
+++ main.go
@@ -3,33 +3,68 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+	"strconv"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "got / request\n")
-	io.WriteString(w, "This is a test\n")
-}
-
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
-}
-
 func main() {
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
-	http.ListenAndServe(":8080", nil)
-	fmt.Println("Server is running on port 8080")
 
+	http.HandleFunc("/calc", func(w http.ResponseWriter, r *http.Request) {
+
+		text := r.URL.Query().Get("text")
+		if text == "" {
+			text = "default input"
+		}
+
+		if isAddOrSub(text) == "add" {
+			numbers, _ := parseExpression(text)
+			result := add(numbers[0], numbers[1])
+			fmt.Fprintf(w, "Result: %d\n", result)
+		} else if isAddOrSub(text) == "sub" {
+			numbers, _ := parseExpression(text)
+			result := sub(numbers[0], numbers[1])
+			fmt.Fprintf(w, "Result: %d\n", result)
+		}
+	})
 	err := http.ListenAndServe(":8080", nil)
-
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Println("Server is shut down")
+		fmt.Println("Server closed")
 	} else if err != nil {
-		fmt.Println("Server is not shut down")
+		fmt.Printf("Error starting server %s\n", err)
 		os.Exit(1)
+	}
+
+}
+
+func add(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func parseExpression(text string) ([]int, []rune) {
+	var numbers []int
+	var chars []rune
+
+	for _, char := range text {
+		if char == '+' || char == '-' {
+			chars = append(chars, char)
+		} else {
+			number, _ := strconv.Atoi(string(char))
+			numbers = append(numbers, number)
+		}
+	}
+	return numbers, chars
+}
+
+func isAddOrSub(text string) string {
+	_, char := parseExpression(text)
+	if char[0] == '+' {
+		return "add"
+	} else {
+		return "sub"
 	}
 }
